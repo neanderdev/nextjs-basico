e5415cf0f93041eba6e7403abd7945d8f7b0a69a
neanderdev <neander.faria@hotmail.com>
2023-01-13 09:57:28 -0300

    API #01

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res
+    .status(200)
+    .json([
+      { name: "John Doe 1" },
+      { name: "John Doe 2" },
+      { name: "John Doe 3" },
+      { name: "John Doe 4" },
+    ]);
 }
