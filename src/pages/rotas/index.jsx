d7342d19d3f2f619dd4f7806c8a7ea90b976f6ad
neanderdev <neander.faria@hotmail.com>
2023-01-13 09:53:05 -0300

    Navegando entre Páginas #02

--- src/pages/rotas/index.jsx
+++ src/pages/rotas/index.jsx
@@ -1,6 +1,21 @@
+import router from 'next/router';
 import Link from 'next/link';
 
-export default function Rotas() {
+export default function Rotas() {    
+    function navegacaoSimples(url) {
+        router.push(url);
+    }
+
+    function navegacaoComParams() {
+        router.push({
+            pathname: '/rotas/params',
+            query: {                
+                id: 10,
+                nome: 'Neander',
+            },            
+        });
+    }
+
     return (
         <div>
             <h1>Rotas Index</h1>
@@ -18,6 +33,26 @@ export default function Rotas() {
                     <li>Neander</li>
                 </Link>
             </ul>
+
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+                <button
+                    onClick={() => router.push('/rotas/10/buscar')}
+                >
+                    Buscar
+                </button>
+
+                <button
+                    onClick={() => navegacaoSimples('/rotas/10/Neander')}
+                >
+                    Neander
+                </button>
+
+                <button
+                    onClick={navegacaoComParams}
+                >
+                    Params
+                </button>
+            </div>
         </div>
     );
 }
