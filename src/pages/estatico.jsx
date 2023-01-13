ee20659bfc86f42618c5b650aadfd088a350f816
neanderdev <neander.faria@hotmail.com>
2023-01-13 10:41:36 -0300

    Estratégias de Renderização

---
+++ src/pages/estatico.jsx
@@ -0,0 +1,15 @@
+export default function Estatico(props) {
+    return (
+        <div>
+            <span>Aleatório: {props.numero}</span>
+        </div>
+    );
+}
+
+export function getStaticProps() {
+    return {
+        props: {
+            numero: Math.random(),
+        },
+    };
+}
